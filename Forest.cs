29f19fb70fc7d36b8929034b6bc734295a08bd49
kidisnotthegoat <165536017+kidisnotthegoat@users.noreply.github.com>
2024-06-26 21:04:10 -0400

    Add files via upload
    just place that into the location folder and then compile it !

---
+++ Forest.cs
@@ -0,0 +1,11 @@
+﻿using GorillaShirts.Interfaces;
+using System;
+
+namespace GorillaShirts.Locations
+{
+    internal class Forest : IStandLocation
+    {
+        public GTZone Zone => GTZone.forest;
+        public Tuple<UnityEngine.Vector3, UnityEngine.Vector3> Location => Tuple.Create<UnityEngine.Vector3, UnityEngine.Vector3>(new(-63.250f, 2.935f, -60f), new(0f, 184.340f, 0f));
+    }
+}
